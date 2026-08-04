2038bf17abab0a314621ec55bac527c85027a47b
MrBraiiv <mikam6285@gmail.com>
2026-08-04 11:21:05 +0300

    Add board data model

---
+++ board.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+
+class Board:
+    def __init__(self, rows):
+        self.rows = rows
+
+class Row:
+    def __init__(self, blocks):
+        self.blocks = blocks
+
+class Block:
+    """Base for Block object types"""
+
+@dataclass
+class ColorBlock(Block):
+    color: str
+    
+
+@dataclass(frozen=True)
+class ResultBlock(Block):
+    pins: tuple[str]
+
